b7980b5243226e56a6e0db16ed96ca1b568a156e
the-refrigerator <65685646+the-refrigerator@users.noreply.github.com>
2025-03-02 21:49:45 +0200

    book details now works properly

--- php/book_details.php
+++ php/book_details.php
@@ -6,7 +6,7 @@ if (isset($_GET['id'])) {
     $book_id = $_GET['id'];
 
     $stmt = $mysqli->prepare('SELECT * FROM Books WHERE isbn = ?');
-    $stmt->bind_param('i', $book_id);
+    $stmt->bind_param('s', $book_id);
     $stmt->execute();
     $result = $stmt->get_result();
     $book = $result->fetch_assoc();
